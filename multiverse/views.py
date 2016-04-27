7cea18da20214c87c63edcb6136dade4a0ee1f00
Moksh <moksh94@icloud.com>
2016-04-27 12:42:30 -0500

    Modified templates to display posts from database.

--- multiverse/views.py
+++ multiverse/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import story_document
 
 # Create your views here.
 
 def home(request):
-	return render(request, 'multiverse/home.html',{})
+	story = story_document.objects.all()
+	return render(request, 'multiverse/home.html',{'story':story})
 
 
